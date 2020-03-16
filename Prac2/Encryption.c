2f661145aac425e56ab35332fba7940893d62bd6
Raymond Walters <u17017051@tuks.co.za>
2020-03-16 18:20:20 +0200

    Converted char strings to integers for hardware optimization

--- Prac2/Encryption.c
+++ Prac2/Encryption.c
@@ -7,99 +7,98 @@
 
 int main(int argc, char *argv[])
 {
+    int i;
     int state_size = 4;
     int message_len = -1;
     int num_blocks = -1;
     int padding_pos = -1;
     int current_block = 0;
     int message_pos = 0;
-    char state_array[MAX_REQ_LEN/(state_size*state_size)][state_size][state_size][3]; //[row][col][hex_byte]
-    char message[MAX_REQ_LEN] = "test functionality";
-    char test[4][3] = {"3A", "65", "71", "1B"};
+    int state_array[MAX_REQ_LEN /
+                    (state_size * state_size)][state_size][state_size];
+    char message[MAX_REQ_LEN];
+    int test[4] = {0x3A, 0x65, 0x71, 0x1B};
 
     word_rotate_32(test);
 
     // Greeting
     printf("EHN 410 Group 12 Practical 2\n\n");
     // To be encrypted
+    for(i = 0; i < MAX_REQ_LEN; i++)
+        message[i] = '\0';
+    strcpy(message, "test functionality");
     //fgets(message, MAX_REQ_LEN, stdin);
     message_len = strlen(message);
-//    blockify16(message, state_array[0], state_size);
 
-    num_blocks = message_len/16;
-    if (message_len%16 != 0){
+    num_blocks = message_len / 16;
+    if(message_len % 16 != 0)
         num_blocks++;
-    }
 
-
-
-    for (current_block = 0; current_block < num_blocks; ++current_block) {
+    for(current_block = 0; current_block < num_blocks; current_block++)
+    {
         blockify_16(message, state_array[current_block], message_pos);
         message_pos += 16;
         print_block_16(state_array[current_block]);
     }
 
-//    if(message_len%16 != 0){
-//
-////        for (padding_pos = message_len; padding_pos < 16*(num_blocks+1); ++padding_pos) {
-////            message[padding_pos] = '';
-////        }
-//        message[16*(num_blocks+1)] = '\0';
-//
-//        blockify16(message, state_array[0], 0);
-//
-////        int byte_pos = 0;
-////        int row, col;
-////        for (col = 0; col < state_size; col++) {
-////            for (row = 0; row < state_size; row++) {
-////                sprintf((char*)(state_array[0][row][col]),"%02X", message[byte_pos]);
-////                byte_pos++;
-////            }
-////        }
-//    }
+/*    if(message_len%16 != 0){
 
+//        for (padding_pos = message_len; padding_pos < 16*(num_blocks+1); ++padding_pos) {
+//            message[padding_pos] = '';
+//        }
+        message[16*(num_blocks+1)] = '\0';
 
+        blockify16(message, state_array[0], 0);
 
-
+//        int byte_pos = 0;
+//        int row, col;
+//        for (col = 0; col < state_size; col++) {
+//            for (row = 0; row < state_size; row++) {
+//                sprintf((char*)(state_array[0][row][col]),"%02X", message[byte_pos]);
+//                byte_pos++;
+//            }
+//        }
+    }*/
 
 }
 
-
-char *pad_bits(char *block, int desired_length){
-    int block_len = strlen(block);
-
-}
-
-
-
-void blockify_16(char *in_message, char state_output[4][4][3], int start_pos){
+void blockify_16(char *in_message, int state_output[4][4], int start_pos)
+{
     int byte_pos = start_pos;
     int row, col;
-    for (col = 0; col < 4; ++col) {
-        for (row = 0; row < 4; ++row) {
-            sprintf((char*)(state_output[row][col]),"%02X", in_message[byte_pos]);
+    for(col = 0; col < 4; col++)
+    {
+        for(row = 0; row < 4; row++)
+        {
+            state_output[row][col] = in_message[byte_pos];
             byte_pos++;
         }
     }
 }
 
-void print_block_16(char state_output[4][4][3]){
+
+void print_block_16(int state_output[4][4])
+{
     int row, col;
-    for (row = 0; row < 4; ++row) {
-        for (col = 0; col < 4; ++col) {
-           printf(state_output[row][col]);
-           printf(" ");
+    for(row = 0; row < 4; row++)
+    {
+        for(col = 0; col < 4; col++)
+        {
+            printf("%02X", state_output[row][col]);
+            printf(" ");
         }
         printf("\n");
     }
     printf("\n");
 }
 
-void word_rotate_32(char word[4][3]){
-    char temp[3];
-    strcpy(temp, word[3]);
-    strcpy(word[3], word[0]);
-    strcpy(word[0], word[1]);
-    strcpy(word[1], word[2]);
-    strcpy(word[2], temp);
+
+void word_rotate_32(int word[4])
+{
+    // Shift last item to front
+    int temp = word[3];
+    word[3] = word[2];
+    word[2] = word[1];
+    word[1] = word[0];
+    word[0] = temp;
 }
